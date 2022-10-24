b071802b4106d1eb915806a614bbc86f0e58899d
Abdulsamet Kaya <aasametkaya@gmail.com>
2022-10-25 00:02:08 +0300

    frames

---
+++ src/api/user-service.js
@@ -0,0 +1,4 @@
+import { settings } from "../utils/settings";
+import axios from "axios";
+
+const API_URL = settings.API_URL;
---
+++ src/components/users/common/header/user-menu.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./user-menu.scss";
+
+const UserMenu = () => {
+  
+};
+
+export default UserMenu;
